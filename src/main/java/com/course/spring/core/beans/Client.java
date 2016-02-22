58952450d3b626caadd4bd5846e5da65220b5325
Vitalii Chechel <vitalii_chechel@epam.com>
2016-02-22 17:49:35 +0200

    1.8 More Context Features

--- src/main/java/com/course/spring/core/beans/Client.java
+++ src/main/java/com/course/spring/core/beans/Client.java
@@ -2,10 +2,12 @@ package com.course.spring.core.beans;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@Getter@Setter@AllArgsConstructor
+@Getter@Setter@RequiredArgsConstructor
 public class Client {
-    private String id;
-    private String fullName;
+    private final String id;
+    private final String fullName;
+    private String greeting;
 }
